16ee69a41b7de0917da699b4f59b182d94be2544
Michael Palmer <michael@michaeldpalmer.com>
2016-10-30 18:11:20 -0400

    Implemented the template. still need to get icons working

--- client/main.js
+++ client/main.js
@@ -1,8 +1,6 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
-import './main.html';
-
 // Stylesheets
 import 'gentelella/vendors/bootstrap/dist/css/bootstrap.min.css';
 import 'gentelella/vendors/font-awesome/css/font-awesome.min.css';
@@ -13,28 +11,33 @@ import 'gentelella/build/css/custom.min.css';
 
 // JavaScript
 import 'gentelella/vendors/jquery/dist/jquery.min'
-import 'gentelella/vendors/jquery/dist/jquery.min.js';
-import 'gentelella/vendors/bootstrap/dist/js/bootstrap.min.js';
-import 'gentelella/vendors/fastclick/lib/fastclick.js';
-import 'gentelella/vendors/nprogress/nprogress.js';
-import 'gentelella/vendors/Chart.js/dist/Chart.min.js';
-import 'gentelella/vendors/bernii/gauge.js/dist/gauge.min.js';
-import 'gentelella/vendors/bootstrap-progressbar/bootstrap-progressbar.min.js';
-import 'gentelella/vendors/iCheck/icheck.min.js';
-import 'gentelella/vendors/skycons/skycons.js';
-import 'gentelella/vendors/Flot/jquery.flot.js';
-import 'gentelella/vendors/Flot/jquery.flot.pie.js';
-import 'gentelella/vendors/Flot/jquery.flot.time.js';
-import 'gentelella/vendors/Flot/jquery.flot.stack.js';
-import 'gentelella/vendors/Flot/jquery.flot.resize.js';
-import 'gentelella/vendors/Flot/jquery.flot.orderBars.js';
-import 'gentelella/flot/date.js';
-import 'gentelella/flot/jquery.flot.spline.js';
-import 'gentelella/flot/curvedLines.js';
-import 'gentelella/maps/jquery-jvectormap-2.0.3.min.js';
-import 'gentelella/moment/moment.min.js';
-import 'gentelella/datepicker/daterangepicker.js';
-import 'gentelella/build/js/custom.min.js';
+import 'gentelella/vendors/bootstrap/dist/js/bootstrap.min';
+import 'gentelella/vendors/fastclick/lib/fastclick';
+import 'gentelella/vendors/nprogress/nprogress';
+import 'gentelella/vendors/Chart.js/dist/Chart.min';
+import 'gentelella/vendors/bernii/gauge.js/dist/gauge.min';
+import 'gentelella/vendors/bootstrap-progressbar/bootstrap-progressbar.min';
+import 'gentelella/vendors/iCheck/icheck.min';
+import 'gentelella/vendors/skycons/skycons';
+import 'gentelella/vendors/Flot/jquery.flot';
+import 'gentelella/vendors/Flot/jquery.flot.pie';
+import 'gentelella/vendors/Flot/jquery.flot.time';
+import 'gentelella/vendors/Flot/jquery.flot.stack';
+import 'gentelella/vendors/Flot/jquery.flot.resize';
+import 'gentelella/production/js/flot/jquery.flot.orderBars';
+import 'gentelella/production/js/flot/date';
+import 'gentelella/production/js/flot/jquery.flot.spline';
+import 'gentelella/production/js/flot/curvedLines';
+import 'gentelella/production/js/maps/jquery-jvectormap-2.0.3.min';
+import 'gentelella/production/js/moment/moment.min';
+// import 'gentelella/production/js/datepicker/daterangepicker';
+import 'gentelella/build/js/custom.min';
+
+import 'gentelella/production/js/maps/jquery-jvectormap-world-mill-en';
+import 'gentelella/production/js/maps/jquery-jvectormap-us-aea-en';
+import 'gentelella/production/js/maps/gdp-data';
+
+import './main.html';
 
 
 Template.hello.onCreated(function helloOnCreated() {
