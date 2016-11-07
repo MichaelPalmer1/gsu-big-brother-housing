b47b7afa1abbe899bc984bd1592fc52fdd3418ff
dtaylor31906 <dt01812@georgiasouthern.edu>
2016-11-07 18:36:20 -0500

    finished basic structure of this script. currently its untested, but iwll fix that soon I added 4 familes please add more families, there is a obvious pattern of how I did the first 4 in the code.

--- public/mongoScripts/dbInit.js
+++ public/mongoScripts/dbInit.js
@@ -8,22 +8,22 @@ var db = conn.getDB("BigBrother");
 var apartment;
 for(var i = 1; i< 13; i++);//make 12 apartments
 {
-    apartment = {"aptNumber": i, "lease": null, "residents": []};
+    apartment = {"_id": i, "lease": null, "residents": []};
     db.Apartments.save(apartment);
 }
 //
 //Construct families that make up residents
-    //Family 1, Single Mom with teenage kid, Apartment 1
+ //Family 1, Single Mom with teenage kid, Apartment 1
     var mom =
     {
         "firstName" : "Erika",
-        "lastName" : "Romagnoli",
+        "lastName" : "Roman",
         "sex" :"female",
         "dateOfBirth" : new Date("1975-12-08"),
         "race" : "Black",
         "occupation" : "Software Developer",
-        "salary": "88000.00",
-        "apartment" : "1",
+        "salary": 88000.00,
+        "apartment" : 1,
         "timeIn" : [],
         "timeOut" : []
 
@@ -31,11 +31,11 @@ for(var i = 1; i< 13; i++);//make 12 apartments
     var son =
     {
         "firstName" : "David",
-        "lastName" : "Romagnoli",
+        "lastName" : "Roman",
         "sex" :"male",
         "dateOfBirth" : new Date("2000-06-21"),
         "race" : "Black",
-        "apartment" : "1",
+        "apartment" : 1,
         "timeIn" : [],
         "timeOut" : []
     };
@@ -43,16 +43,180 @@ for(var i = 1; i< 13; i++);//make 12 apartments
     db.Residents.save(mom);
     db.Residents.save(son);
 
-    var signer = db.Residents.find({"firstName" : "Erika", "lastName" : "Romagnoli"},{});//should return object id
+    var signer = db.Residents.find({"firstName" : "Erika", "lastName" : "Roman"},{});//should return object id
 
     db.Leases.save(
         {
-            "rent" : "735.22",
-            "aparment" : "1",
-            "startDate" : new Date("2016-01-5"),
-            "endDate" : new Date("2017-01-5"),
+            "rent" : 735.22,
+            "apartment" : 1,
+            "startDate" : new Date("2016-01-5T00:00:00"),
+            "endDate" : new Date("2017-01-5T00:00:00"),
             "signer" : signer
     });
-//
+
+//Family 2, Single Woman with no kids 
+    var woman =
+    {
+        "firstName" : "Rhonda",
+        "lastName" : "James",
+        "sex" :"female",
+        "dateOfBirth" : new Date("1995-10-18"),
+        "race" : "White",
+        "occupation" : "Bartender",
+        "salary": 48000.00,
+        "apartment" : 2,
+        "timeIn" : [],
+        "timeOut" : []
+
+    };
+
+    db.Residents.save(woman);    
+
+    signer = db.Residents.find({"firstName" : "Rhonda", "lastName" : "James"},{});//should return object id
+
+    db.Leases.save(
+        {
+            "rent" : 735.22,
+            "apartment" : 2,
+            "startDate" : new Date("2016-05-15T00:00:00"),
+            "endDate" : new Date("2017-05-15T00:00:00"),
+            "signer" : signer
+    });
+
+//Family 3, Mom, Dad, daughter
+    var mom =
+    {
+        "firstName" : "Ana",
+        "lastName" : "Reindhart",
+        "sex" :"female",
+        "dateOfBirth" : new Date("1990-09-27"),
+        "race" : "White",
+        "occupation" : "Paramedic",
+        "salary": 43000.00,
+        "apartment" : 3,
+        "timeIn" : [],
+        "timeOut" : []
+
+    };
+     var dad =
+    {
+        "firstName" : "Leo",
+        "lastName" : Reindhart,
+        "sex" :"male",
+        "dateOfBirth" : new Date("1988-07-03"),
+        "race" : "White",
+        "occupation" : "Demolition Specialist",
+        "salary": 47000.00,
+        "apartment" : 3,
+        "timeIn" : [],
+        "timeOut" : []
+
+    };
+
+     var babyGirl =
+    {
+        "firstName" : "Mercy",
+        "lastName" : "Reindhart",
+        "sex" :"female",
+        "dateOfBirth" : new Date("2013-04-09"),
+        "race" : "White",        
+        "apartment" : 3,
+        "timeIn" : [],
+        "timeOut" : []
+
+    };
+
+    db.Residents.save(mom);
+    db.Residents.save(dad); 
+    db.Residents.save(babyGirl);   
+
+    signer = db.Residents.find({"firstName" : dad.firstName, "lastName" : dad.lastName},{});//should return object id
+
+    db.Leases.save(
+        {
+            "rent" : 735.22,
+            "apartment" : 3,
+            "startDate" : new Date("2016-04-09T00:00:00"),
+            "endDate" : new Date("2017-04-09T00:00:00"),
+            "signer" : signer
+    });
+
+//Family 4, 2 brothers and a friend
+        var older =
+    {
+        "firstName" : "Hanzo",
+        "lastName" : "Shimada",
+        "sex" :"male",
+        "dateOfBirth" : new Date("1989-02-08"),
+        "race" : "Asian",
+        "occupation" : "Archery Expert",
+        "salary": 29000.00,
+        "apartment" : 4,
+        "timeIn" : [],
+        "timeOut" : []
+
+    };
+        var younger =
+    {
+        "firstName" : "Genji",
+        "lastName" : "Shimada",
+        "sex" :"male",
+        "dateOfBirth" : new Date("1991-03-08"),
+        "race" : "Asian",
+        "occupation" : "Ninjust Instructor",
+        "salary": 32000.00,
+        "apartment" : 4,
+        "timeIn" : [],
+        "timeOut" : []
+
+    };
+
+        var friend =
+    {
+        "firstName" : "Tekha",
+        "lastName" : "Zenyatta",
+        "sex" :"male",
+        "dateOfBirth" : new Date("1992-02-08"),
+        "race" : "Asian",
+        "occupation" : "Tai Chi Fighter",
+        "salary": 40000.00,
+        "apartment" : 4,
+        "timeIn" : [],
+        "timeOut" : []
+
+    };
+    
+
+    db.Residents.save(woman);    
+
+    signer = db.Residents.find({"firstName" : older.firstName, "lastName" : older.lastName});//should return object id
+
+    db.Leases.save(
+        {
+            "rent" : 735.22,
+            "apartment" : 4,
+            "startDate" : new Date("2016-05-15T00:00:00"),
+            "endDate" : new Date("2017-05-15T00:00:00"),
+            "signer" : signer
+    });
+
+    //Add additional familes here if you keep the same format the residents will be added to the apartments list automatically
+
 //Add residents to apartments list
+var leases = db.Leases.find({"endDate" : {$gt: Date()}}).foreach(addResidents);//leases that havet expired 
+function addResidents(lease)
+{
+    //grab its apartment from database
+     var apartment = db.Apartment.find({"_id": lease.apartment})
+     //grab array of people who now stay there
+     var residents = db.Residents.find({"apartment": lease.apartment},{"_id": 1 ,"firstName" : 1, "lastName" :1}).toArray();
+     //set that array up in the apt
+     db.Apartment.update({"_id": lease.apartment}, {$set:{"residents": residents}})
+
+     var firstTimeIn = lease.startDate;
+     for(var i = 0; i < residents.length; i++)
+     {
+        db.Residents.update({"_id" : residents[i]._id}, {$set:{"timeIn.0":firstTimeIn}});      
+     }
+}
 //
