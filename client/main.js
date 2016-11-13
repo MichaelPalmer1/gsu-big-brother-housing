e6f9a0bd36dee4b42aa3b71ad2fa565060aba4c4
carvey <j95.charles@gmail.com>
2016-11-13 17:34:58 -0500

    Added table on home page

--- client/main.js
+++ client/main.js
@@ -4,19 +4,19 @@
 // import './main.html';
 //import './index.html';
 
-import 'moment';
+//import 'moment';
 
-import '../public/vendors/Chart.js/dist/Chart.min.js';
-import '../public/vendors/gauge.js/dist/gauge.min.js';
-import '../public/vendors/bootstrap-progressbar/bootstrap-progressbar.min.js';
-import '../public/vendors/iCheck/icheck.min.js';
-import '../public/vendors/skycons/skycons.js';
-import '../public/vendors/Flot/jquery.flot.js';
-import '../public/vendors/Flot/jquery.flot.pie.js';
-import '../public/vendors/Flot/jquery.flot.time.js';
-import '../public/vendors/Flot/jquery.flot.stack.js';
-import '../public/vendors/Flot/jquery.flot.resize.js';
-// import 'gentelella/vendors/Flot/jquery.flot.orderBars.js';
+//import '../public/vendors/Chart.js/dist/Chart.min.js';
+//import '../public/vendors/gauge.js/dist/gauge.min.js';
+//import '../public/vendors/bootstrap-progressbar/bootstrap-progressbar.min.js';
+//import '../public/vendors/iCheck/icheck.min.js';
+//import '../public/vendors/skycons/skycons.js';
+//import '../public/vendors/Flot/jquery.flot.js';
+//import '../public/vendors/Flot/jquery.flot.pie.js';
+//import '../public/vendors/Flot/jquery.flot.time.js';
+//import '../public/vendors/Flot/jquery.flot.stack.js';
+//import '../public/vendors/Flot/jquery.flot.resize.js';
+//// import 'gentelella/vendors/Flot/jquery.flot.orderBars.js';
 // import 'gentelella/flot/date.js';
 // import 'gentelella/flot/jquery.flot.spline.js';
 // import 'gentelella/flot/curvedLines.js';
