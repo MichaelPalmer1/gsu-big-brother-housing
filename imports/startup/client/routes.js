b1e42289a30fcac893568d2acc3be5026bccedab
carvey <j95.charles@gmail.com>
2016-11-15 16:25:54 -0500

    Updated some things so they now work again

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -5,15 +5,15 @@ import '../../ui/layouts/main_layout/main.html';
 import '../../ui/pages/home/home.html';
 import '../../ui/pages/admin/admin.html';
 import '../../ui/pages/error/error.html';
-import '../../ui/pages/residents/listing/residents.html';
-import '../../ui/pages/residents/add/add_resident.html';
+//import '../../ui/pages/residents/listing/residents.html';
+//import '../../ui/pages/residents/add/add_resident.html';
 
 // javascript files
 import '../../ui/pages/home/home';
 import '../../ui/pages/admin/admin';
 import '../../ui/pages/error/error';
-import '../../ui/pages/residents/add/add_residents';
-import '../../ui/pages/residents/listing/residents';
+//import '../../ui/pages/residents/add/add_residents';
+//import '../../ui/pages/residents/listing/residents';
 
 
 Router.configure({
