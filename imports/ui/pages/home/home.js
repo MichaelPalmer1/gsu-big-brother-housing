2932e5b0b84d47b7e4d0bccc5e6419891cd419c8
carvey <j95.charles@gmail.com>
2016-11-15 17:07:25 -0500

    Moved some tables around and got rid of body tags in residents.html

--- imports/ui/pages/home/home.js
+++ imports/ui/pages/home/home.js
@@ -1,7 +1,4 @@
 import './home.html';
 
-import '../../components/residents/residents.html';
-import '../../components/residents/residents';
-
 import '../../components/apartments/apartments.html';
 import '../../components/apartments/apartments';
--- imports/ui/pages/residents/listing/residents.js
+++ imports/ui/pages/residents/listing/residents.js
@@ -1,1 +1,4 @@
 import './residents.html';
+
+import '../../../components/residents/residents.html';
+import '../../../components/residents/residents';
