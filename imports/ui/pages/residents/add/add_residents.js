b276c885198307fe46cb017caf481c6b4eb9ed3e
carvey <j95.charles@gmail.com>
2016-11-27 01:22:30 -0500

    Patched up the form a bit. Still needs the submission logic

--- imports/ui/pages/residents/add/add_residents.js
+++ imports/ui/pages/residents/add/add_residents.js
@@ -1,1 +1,25 @@
 import './add_resident.html';
+
+Template.add_resident.rendered = function() {
+
+    $.getScript('/vendors/jQuery-Smart-Wizard/js/jquery.smartWizard.js', function(){
+        $('#wizard').smartWizard();
+
+
+        $('.buttonNext').addClass('btn btn-success');
+        $('.buttonPrevious').addClass('btn btn-primary');
+        $('.buttonFinish').addClass('btn btn-default');
+
+    });
+
+
+
+};
+
+//Template.add_resident.events({
+//   "click .buttonFinish": function(event) {
+//        event.preventDefault();
+//       console.log(event);
+//   }
+//});
+
