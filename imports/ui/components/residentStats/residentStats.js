5337326ce2760be9f9bab24dbf99f24b6804084d
carvey <j95.charles@gmail.com>
2016-11-27 16:56:24 -0500

    Present stats work on home page

--- imports/ui/components/residentStats/residentStats.js
+++ imports/ui/components/residentStats/residentStats.js
@@ -7,5 +7,14 @@ import './residentStats.css';
 Template.residentStats.helpers({
     resident_count: function() {
         return Residents.find().count();
+    },
+    residents_present: function() {
+        return Residents.find({'present': true}).count();
+    },
+    residents_absent: function() {
+        return Residents.find({'present': false}).count();
+    },
+    percent_present: function() {
+        return (Residents.find({'present': true}).count() / Residents.find().count()) * 100;
     }
 });
